a3b219e4fa4b9960199fbd9cc4c63df97ebb789b
Rafael Ortiz Zableh <57945332+rafaelortizzableh@users.noreply.github.com>
2023-09-27 19:26:15 +0200

    chore: fix linter rules and extract out of the build method

--- lib/widgets/button_first_level.dart
+++ lib/widgets/button_first_level.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+
 import 'button_widget.dart';
 
 class ButtonFirstLevel extends StatelessWidget {
-  const ButtonFirstLevel();
+  const ButtonFirstLevel({super.key});
+
+  static final _backgroundColor = Colors.blueGrey[400] ?? Colors.blueGrey;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueGrey[400],
-      child: ButtonWidget(),
+    return ColoredBox(
+      color: _backgroundColor,
+      child: const ButtonWidget(),
     );
   }
 }
--- lib/widgets/counter_widget.dart
+++ lib/widgets/counter_widget.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
 class CounterWidget extends StatelessWidget {
-  const CounterWidget();
+  const CounterWidget({super.key});
+
+  static final _backgroundColor = Colors.blue[800] ?? Colors.blue;
+  static const _foregroundColor = Colors.white;
+  static const _fontSize = 18.0;
+  static const _externalPadding = EdgeInsets.all(30.0);
+  static const _internalPadding = EdgeInsets.all(10.0);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(30.0),
-      child: Container(
-        color: Colors.blue[800],
-        child: Center(
+      padding: _externalPadding,
+      child: ColoredBox(
+        color: _backgroundColor,
+        child: const Center(
           child: Padding(
-            padding: const EdgeInsets.all(10.0),
+            padding: _internalPadding,
             child: Text(
               '28',
-              style: const TextStyle(
-                fontSize: 18.0,
+              style: TextStyle(
+                fontSize: _fontSize,
+                color: _foregroundColor,
               ),
             ),
           ),
